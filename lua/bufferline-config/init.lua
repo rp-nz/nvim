73d4d7b74e6e5da7d3dae1b5fa550664e89c2bbd
rpnz <ritesh@itguru.co.nz>
2021-10-24 23:38:36 +1300

    fix paste and bufferline issue

--- lua/bufferline-config/init.lua
+++ lua/bufferline-config/init.lua
@@ -1,5 +1,5 @@
 require("bufferline").setup{}
 vim.cmd[[
 nnoremap <silent>nb :BufferLineCycleNext<CR>
-nnoremap <silent>pb :BufferLineCyclePrev<CR>
+nnoremap <silent>ob :BufferLineCyclePrev<CR>
 ]]
